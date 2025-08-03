1e19a587fe48010963a46e4ff5ef438372fdc10b
shivam <shivampandey02080@gmail.com>
2025-08-03 23:44:42 +0530

    03react

---
+++ 03react/src/component/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+function Card(){
+    return(
+        <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600 mb-10">
+        Welcome to Tailwind + Vite + React
+      </h1>
+      <p className="text-lg text-gray-700">
+        Your Tailwind CSS setup is working perfectly!
+      </p>
+      <button className="mt-6 px-6 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition">
+        Click Me
+      </button>
+    </div>
+    )
+}
+export default Card
